4a3f997e652258f387f80a0aa245dfc87548c071
aaronersando <agersando@gmail.com>
2024-09-05 14:19:21 +0800

    input field, text, siri wave design added

--- www/main.js
+++ www/main.js
@@ -10,4 +10,15 @@ $(document).ready(function () {
             effect: 'bounceOut',
         },
     })
+
+    // Siri wave output
+    var siriWave = new SiriWave({
+        container: document.getElementById("siri-container"),
+        width: 900,
+        height: 300,
+        style: "ios",
+        amplitude: "1",
+        speed: "0.20",
+        autostart: true,
+      });
 });
